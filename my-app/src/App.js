eb73a9d2ed298527e48d0751dc018d2c5eff51a9
16519111 <gregdimasbaskara@gmail.com>
2021-02-09 13:40:54 +0700

    Install Material Styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
