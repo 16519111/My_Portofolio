679e0d3d9e124f28bb8e245823a8049502b20418
16519111 <gregdimasbaskara@gmail.com>
2021-02-11 18:56:54 +0700

    Experience

--- my-app/src/components/Experience/Experience.js
+++ my-app/src/components/Experience/Experience.js
@@ -1,23 +1,222 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Accordion, AccordionDetails, AccordionSummary, Typography, Divider } from '@material-ui/core'
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { AppBar, Tabs, Tab, Box, Typography, Divider, Step, Stepper, StepLabel, Button } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  backButton: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+function getSteps(page) {
+  if (page === 1) {
+    return ['Tutoring and Training', 'First Task', 'First Feature', 'Second Feature', 'Third Feature', 'Fourth Feature'];
+  } else if (page === 2) {
+    return ['First Meeting', 'First Task : Main Landing Page', 'IT Fest Page'];
+  } else {
+    return ['First Meeting', 'Landing Page', 'Preevent Page', 'Details & Context'];
+  }
+}
+
+function getStepContent(stepIndex) {
+  switch (stepIndex) {
+    case 0:
+      return 'Select campaign settings...';
+    case 1:
+      return 'What is an ad group anyways?';
+    case 2:
+      return 'This is the bit I really care about!';
+    default:
+      return 'Unknown stepIndex';
+  }
+}
 
 function Experience() {
-    const classes = useStyles();
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
     return (
       <div className={classes.root}>
-        <Typography variant="h4" align="center" style={{marginTop: "2%"}}>Websites</Typography>
+        <Typography variant="h4" align="center" style={{marginTop: "2%"}}>Experiences</Typography>
+        <Divider style={{marginTop: "2%", marginLeft: "10%", marginRight: "10%", marginBottom: "2%"}}/>
+        <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Schooly System" {...a11yProps(0)} />
+          <Tab label="Arkavidia" {...a11yProps(1)} />
+          <Tab label="StudentxCEOs" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          <Stepper activeStep={activeStep} alternativeLabel>
+            {getSteps(1).map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <div style={{display: "flex", justifyContent: "center"}}>
+            {activeStep === getSteps(1).length ? (
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Typography className={classes.instructions}>All steps completed</Typography>
+                <Button onClick={handleReset}>Reset</Button>
+              </div>
+            ) : (
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                <div>
+                  <Button
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    className={classes.backButton}
+                  >
+                    Back
+                  </Button>
+                  <Button variant="contained" color="primary" onClick={handleNext}>
+                    {activeStep === getSteps(1).length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+        <Stepper activeStep={activeStep} alternativeLabel>
+            {getSteps(2).map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <div style={{display: "flex", justifyContent: "center"}}> 
+            {activeStep === getSteps(2).length ? (
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Typography className={classes.instructions}>All steps completed</Typography>
+                <Button onClick={handleReset}>Reset</Button>
+              </div>
+            ) : (
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                <div>
+                  <Button
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    className={classes.backButton}
+                  >
+                    Back
+                  </Button>
+                  <Button variant="contained" color="primary" onClick={handleNext}>
+                    {activeStep === getSteps(2).length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+        <Stepper activeStep={activeStep} alternativeLabel>
+            {getSteps(3).map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <div style={{display: "flex", justifyContent: "center"}}>
+            {activeStep === getSteps(3).length ? (
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Typography className={classes.instructions}>All steps completed</Typography>
+                <Button onClick={handleReset}>Reset</Button>
+              </div>
+            ) : (
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                <div style={{display: "flex", justifyContent: "center"}}>
+                  <Button
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    className={classes.backButton}
+                  >
+                    Back
+                  </Button>
+                  <Button variant="contained" color="primary" onClick={handleNext}>
+                    {activeStep === getSteps(3).length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+        </TabPanel>
       </div>
     );
   }
--- my-app/src/components/Projects/Project.js
+++ my-app/src/components/Projects/Project.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Accordion, AccordionDetails, AccordionSummary, Typography, Divider } from '@material-ui/core'
+import { Accordion, AccordionDetails, AccordionSummary, Typography, Divider, Avatar, Grid, Paper, Button } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SXCPicture from "../../images/SXC.png";
+import JLogoPicture from "../../images/Logo.ico";
+import GamePicture from "../../images/Game.png";
+import ArkavidiaPicture from "../../images/Arkavidia.png";
+import ReactPicture from "../../images/React.png";
+import PythonPicture from "../../images/Python.png";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,110 +17,184 @@ const useStyles = makeStyles((theme) => ({
         fontSize: theme.typography.pxToRem(15),
         fontWeight: theme.typography.fontWeightRegular,
     },
+    avatar: {
+        width: "100px",
+        height: "100px",
+    },
+    flexRowAvatar: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around"
+    },
+    paper: {
+        padding: "20px",
+        marginTop: "10px",
+        marginBottom: "10px",
+        display: "flex",
+        flexDirection: "row",
+        flex: "1"
+    }
   }));
 
 function Projects() {
     const classes = useStyles();
+    const [topic, setTopic] = React.useState(1)
+
+    const handleChangeTopic = (topic) => {
+        setTopic(topic)
+    }   
+
     return (
       <div className={classes.root}>
-        <Typography variant="h4" align="center" style={{marginTop: "2%"}}>Websites</Typography>
-        <Divider style={{marginTop: "2%", marginLeft: "10%", marginRight: "10%", marginBottom: "2%"}}/>
-        <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-            <Typography className={classes.heading}>StudentxCEOs 10th Grand Summit</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-            <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                sit amet blandit leo lobortis eget.
-            </Typography>
-            </AccordionDetails>
-        </Accordion>
-        <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-            <Typography className={classes.heading}>Web Search Engine</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-            <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                sit amet blandit leo lobortis eget.
-            </Typography>
-            </AccordionDetails>
-        </Accordion>
-        <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-            <Typography className={classes.heading}>Movies and Games Catalog</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-            <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                sit amet blandit leo lobortis eget.
-            </Typography>
-            </AccordionDetails>
-        </Accordion>
-        <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-            <Typography className={classes.heading}>Arkavidia 7.0</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-            <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                sit amet blandit leo lobortis eget.
-            </Typography>
-            </AccordionDetails>
-        </Accordion>
-        <Typography variant="h4" align="center" style={{marginTop: "6%"}}>Mobile Apps</Typography>
-        <Divider style={{marginTop: "2%", marginLeft: "10%", marginRight: "10%", marginBottom: "2%"}}/>
-        <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-            <Typography className={classes.heading}>Indonesia COVID19 Cases TrackerApp</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-            <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                sit amet blandit leo lobortis eget.
-            </Typography>
-            </AccordionDetails>
-        </Accordion>
-        <Typography variant="h4" align="center" style={{marginTop: "6%"}}>Data Science</Typography>
-        <Divider style={{marginTop: "2%", marginLeft: "10%", marginRight: "10%", marginBottom: "2%"}}/>
-        <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-            <Typography className={classes.heading}>COVID-19 Cases Detection Using Convolutional
-Neural Network Image Classification (Deep
-Learning) on X-Ray (Thorax) Samples</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-            <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                sit amet blandit leo lobortis eget.
-            </Typography>
-            </AccordionDetails>
-        </Accordion>
+        <Typography variant="h4" align="center" style={{marginTop: "2%"}}>
+            {(topic === 1) ? "Websites" : (topic === 2) ? "Mobile Apps" : "Data Science"}
+        </Typography>
+        <div style={{display: "flex", justifyContent: "flex-end", marginLeft: "10%", marginRight: "10%", marginTop: "2%"}}>
+            <Button onClick={() => handleChangeTopic(1)}>Websites</Button>
+            <Button onClick={() => handleChangeTopic(2)}>Mobile Apps</Button>
+            <Button onClick={() => handleChangeTopic(3)}>Data Science</Button>
+        </div>
+        <Divider style={{marginTop: "0%", marginLeft: "10%", marginRight: "10%", marginBottom: "2%"}}/>
+        {(topic === 1) ?
+            <>
+                <Grid container className={classes.flexRowAvatar}>
+                    <Grid item xs={2}></Grid>
+                    <Grid item container xs={8} className={classes.flexRowAvatar}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="row" alignItems="center">
+                                <Grid item xs={2}>
+                                    <Avatar alt="Remy Sharp" src={SXCPicture} className={classes.avatar}/>
+                                </Grid>
+                                <Grid item style={{}} xs={10}>
+                                    <Typography variant="h6" style={{fontWeight: "bold"}} gutterBottom>StudentxCEOs 10th Grand Summit</Typography>
+                                    <Typography variant="body2">The biggest annual event held by StudentsxCEOs Bandung Chapter, a platform for students across Indonesia to collaborate and solve real prominent business cases, problems related to Sustainable Development Goals (SDGs), and the chance to learn and engage with renowned speakers from their respected fields through a series of grand talkshows.</Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                </Grid>
+                <Divider style={{marginTop: "1.5%", marginLeft: "30%", marginRight: "30%", marginBottom: "1.5%"}}/>
+                <Grid container className={classes.flexRowAvatar}>
+                    <Grid item xs={2}></Grid>
+                    <Grid item container xs={8} className={classes.flexRowAvatar}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="row" alignItems="center">
+                                <Grid container item xs={2}>
+                                    <Avatar alt="Remy Sharp" src={JLogoPicture} className={classes.avatar}/>
+                                </Grid>
+                                <Grid item style={{}} xs={10}>
+                                    <Typography variant="h6" style={{fontWeight: "bold"}} gutterBottom>Web Search Engine</Typography>
+                                    <Typography variant="body2" >A Website made with React.js which performs likea Search Engine : Finding and DisplayingDocuments with the closest relation to the Queryinputted by the user. User can upload one or multiple documents atonce, which will then be stemmed and stored in afirebase database. Then, every time the userinsert a query, the web will calculate the cosinesimiliarity using vectors to determine the results</Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                </Grid>
+                <Divider style={{marginTop: "1.5%", marginLeft: "30%", marginRight: "30%", marginBottom: "1.5%"}}/>
+                <Grid container className={classes.flexRowAvatar}>
+                    <Grid item xs={2}></Grid>
+                    <Grid item container xs={8} className={classes.flexRowAvatar}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="row" alignItems="center">
+                                <Grid item xs={2}>
+                                    <Avatar alt="Remy Sharp" src={GamePicture} className={classes.avatar}/>
+                                </Grid>
+                                <Grid item style={{}} xs={10}>
+                                    <Typography variant="h6" style={{fontWeight: "bold"}} gutterBottom>Web-Movies-and-Games-Catalog</Typography>
+                                    <Typography variant="body2">A Website made with React.js which displays listsof favorite games and movies, each with its owndescriptions/details. User can add and delete games or movies, andgive additional details in realtime with other usersafter logging in to the website (must registerfirst). Uses live API and Database.</Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                </Grid>
+                <Divider style={{marginTop: "1.5%", marginLeft: "30%", marginRight: "30%", marginBottom: "1.5%"}}/>
+                <Grid container className={classes.flexRowAvatar}>
+                    <Grid item xs={2}></Grid>
+                    <Grid item container xs={8} className={classes.flexRowAvatar}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="row" alignItems="center">
+                                <Grid item xs={2}>
+                                    <Avatar alt="Remy Sharp" src={ArkavidiaPicture} className={classes.avatar}/>
+                                </Grid>
+                                <Grid item style={{}} xs={10}>
+                                    <Typography variant="h6" style={{fontWeight: "bold"}} gutterBottom>Arkavidia 7.0</Typography>
+                                    <Typography variant="body2">Arkavidia 7.0 is an annual prestigious event organized by Institut Teknologi Bandung's Informatics Student Association (HMIF ITB). Themed "Establishing Digital Independence," Arkavidia is here to provide awareness for every individual of their rights and obligations in the digital world, so that they can realize digital independence for themselves.</Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                </Grid>
+            </>
+        :  (topic === 2) ? 
+            <>
+            <Grid container className={classes.flexRowAvatar}>
+                <Grid item xs={2}></Grid>
+                    <Grid item container xs={8} className={classes.flexRowAvatar}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="row" alignItems="center">
+                                <Grid item xs={2}>
+                                    <Avatar alt="Remy Sharp" src={ReactPicture} className={classes.avatar}/>
+                                </Grid>
+                                <Grid item style={{}} xs={10}>
+                                    <Typography variant="h6" style={{fontWeight: "bold"}} gutterBottom>Indonesia-COVID19-Cases-TrackerApp</Typography>
+                                    <Typography variant="body2">A Mobile App made with React Native that keeps track of COVID-19 Cases in Indonesia per Provincethrough a live API (Postman, REST API).
+Includes a login page, and detail pages whichdisplay the details of each province.</Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                </Grid>
+                <Divider style={{marginTop: "1.5%", marginLeft: "30%", marginRight: "30%", marginBottom: "1.5%"}}/>
+                <Grid container className={classes.flexRowAvatar}>
+                <Grid item xs={2}></Grid>
+                    <Grid item container xs={8} className={classes.flexRowAvatar}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="row" alignItems="center">
+                                <Grid item xs={2}>
+                                    <Avatar alt="Remy Sharp" src={ReactPicture} className={classes.avatar}/>
+                                </Grid>
+                                <Grid item style={{}} xs={10}>
+                                    <Typography variant="h6" style={{fontWeight: "bold"}} gutterBottom>Homey Homecooking Delivery App</Typography>
+                                    <Typography variant="body2">A Mobile App made with React Native and Django Framework that serves as a meeting point between clients and homecooking merchants. (Currently in progress)</Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                </Grid>
+                </>
+            : 
+            <>
+            <Grid container className={classes.flexRowAvatar}>
+                <Grid item xs={2}></Grid>
+                    <Grid item container xs={8} className={classes.flexRowAvatar}>
+                        <Paper className={classes.paper}>
+                            <Grid container direction="row" alignItems="center">
+                                <Grid item xs={2}>
+                                    <Avatar alt="Remy Sharp" src={PythonPicture} className={classes.avatar}/>
+                                </Grid>
+                                <Grid item style={{}} xs={10}>
+                                    <Typography variant="h6" style={{fontWeight: "bold"}} gutterBottom>COVID-19 Cases Detection Using CNN Image Classification</Typography>
+                                    <Typography variant="body2">A Research Project based on Deep Learning and
+Image Classification to provide an alternative to
+swab test for COVID19 testing.
+The Project uses multiple model such as ResNet50
+to Classify whether a patient is COVID Positive
+based on the patient's Thorax X-Ray Sample.</Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                </Grid>
+                
+                </>
+            }
       </div>
     );
   }
